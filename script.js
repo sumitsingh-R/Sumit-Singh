8e50acc259b3f3954799442d9450ad9e1f773f57
Sumit Singh <sumitsinghvlogs1@gmail.com>
2026-08-06 12:34:15 +0530

    my_portfolio

---
+++ script.js
@@ -0,0 +1,45 @@
+/*console.log("javascript is connected");
+let a = 24
+let b = 45
+console.log(a+b);
+if (a%2==0)
+    console.log("even number")
+else
+    console.log("odd number")
+let name = prompt("enter your name")
+console.log("name");
+
+
+for( let i = 1;  i <=10;i++){
+    console.log("5 *"+i+ "=" + (5*i));
+
+}
+let= num = 5;
+let fact = 1;
+for ( let i = 1; i<= num; i++){
+    fact= fact*i
+}
+    console.log("FACTORIAL OF" + num + "=" +fact);*/
+
+   /* let str = prompt("enter your name")
+    let st = ""
+    for(let i = 0; i<str.length;i++){
+        let ch = str.charAt(i)
+        st = ch + st;
+    }*/
+    
+   /* let str = prompt("enter  a string:")
+    let count = 0;
+    for (let ch of str){
+        if (ch === 'a'||
+            ch === 'e'||
+            ch ==='i'||
+            ch === 'o'||
+            ch === 'u'
+        ){
+            count++;
+        }
+
+    }
+    console.log("No of vowels in string:", count);*/
+    let arr = []
